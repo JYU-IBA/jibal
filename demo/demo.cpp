f0a34cd7efd385a045de158a11aab9b4bd245647
Jaakko Julin <jaakko.a.julin@jyu.fi>
2020-05-05 10:32:11 +0300

    Fixed the minimal C++ demo code to work again after plenty of changes.

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -7,13 +7,16 @@ extern "C" {
 };
 
 int main() {
-    jibal *jibal = jibal_init(nullptr);
-    if(!jibal) {
-        std::cout << "Couldn't initialize JIBAL" << std::endl;
+    jibal jibal = jibal_init(nullptr);
+    if(jibal.error) {
+        std::cerr << "Initializing JIBAL failed with error code: "
+            << jibal.error
+            << " (" << jibal_error_string(jibal.error) << ")"
+            << std::endl;
+        return 1;
     }
-    jibal_isotope *isotopes=jibal_isotopes_load(NULL);
-    jibal_isotope *alpha=jibal_isotope_find(isotopes, "4He", 0, 0);
+    jibal_isotope *alpha=jibal_isotope_find(jibal.isotopes, "4He", 0, 0);
     std::cout << "The mass of " << alpha->name << " is " << alpha->mass/C_U << " u" << std::endl;
-    jibal_isotopes_free(isotopes);
+    jibal_free(&jibal);
     return 0;
 }
