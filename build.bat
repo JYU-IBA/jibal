3782e019853660c3603bceb5d97d02b359851a5c
Jaakko Julin <jaakko.a.julin@jyu.fi>
2020-05-14 16:04:22 +0300

    Cleanup.

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ mkdir build
 cd build
 REM In case you run into errors, delete the build directory or at least delete the CMakeCache.txt file
 del CMakeCache.txt
-cmake -G "Visual Studio 15 2017" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ../
+cmake -G "Visual Studio 15 2017" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ../
 msbuild ALL_BUILD.vcxproj
