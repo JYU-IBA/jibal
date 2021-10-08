847d4599b52a08c94d0a3bf20b4d1239f65cc78c
Jaakko Julin <jaakko.julin@jyu.fi>
2021-10-08 08:57:42 +0300

    Changed the build.bat to be more in line with instructions in INSTALL.md

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 REM This script probably doesn't work, but it gives you an idea how the process should work
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\vc\Auxiliary\Build\vcvars64.bat"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
 REM In case you run into errors, delete the build directory or at least delete the CMakeCache.txt file
 del CMakeCache.txt
-cmake -G "Visual Studio 15 2017" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ../
-msbuild ALL_BUILD.vcxproj
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ../
+REM The following will make a package
+cmake --build . --target PACKAGE --config Release
+REM The following (uncommented) will just build
+REM cmake --build . --target ALL_BUILD --config Release
