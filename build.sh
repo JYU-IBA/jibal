c787ad77112fb9c8fc4a6d13d7348b0e39ecd199
Jaakko Julin <jaakko.julin@jyu.fi>
2024-10-14 15:01:32 +0300

    A bit nicer build example.

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-mkdir build
-cd build
-rm CMakeCache.txt
-cmake ../
-make
-#make install
+src_dir="."
+build_dir="build"
+if ! cmake --fresh -S "$src_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE=Release; then
+    echo "Could not configure using CMake"
+    exit 1;
+fi
+
+if ! cmake --build "$build_dir" --config Release; then
+    echo "Could not build."
+    exit 1;
+fi
+
+#if ! sudo cmake --install "$build_dir" then
+#    echo "Could not install."
+#    exit 1;
+#fi
